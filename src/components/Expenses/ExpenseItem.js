e9f65efc0f5dc529818f5dbddb701bcecbcd6e47
Woong <dhksdnd96@naver.com>
2022-04-20 16:30:52 +0900

    Edit and Add button  ExpsenseItem

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,21 +2,28 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
   //   const expenseDate = new Date(2021, 2, 28);
   //   const expenseTitle = 'Car Insurance';
   //   const expenseAmount = 294.67;
+  let title = props.title;
+
+  const clickHandler = () => {
+    title = 'Updated!';
+    console.log('Clicked!!!!');
+  };
 
   return (
     <Card className="expense-item">
       {/* <div>{props.date.toISOString()}</div> */}
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,7 +2,7 @@ import './Expenses.css';
 import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
 
-function Expenses(props) {
+const Expenses = (props) => {
   return (
     <Card className="expenses">
       <ExpenseItem
@@ -27,6 +27,6 @@ function Expenses(props) {
       />
     </Card>
   );
-}
+};
 
 export default Expenses;
